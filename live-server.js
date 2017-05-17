a8ed734d145ae19a80a4d2624980406aa2116801
Oeslei Kuhn <oesleik@gmail.com>
2017-05-17 00:07:33 -0300

    Ajuste no script live-server para rodar em Linux / Windows

--- live-server.js
+++ live-server.js
@@ -1,9 +1,10 @@
 const liveServer = require('live-server')
+const isWindows = /^win/.test(process.platform)
 
 const params = {
   port: 8888,
   host: process.env.IP,
-  root: '/var/www/html/nfov/examples',
+  root: (isWindows ? 'c:/wamp/www/' : '/var/www/html/') + 'nfov/examples',
   open: '/',
   logLevel: 1 // 0 = errors only, 1 = some, 2 = lots
 }
