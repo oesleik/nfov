d75468f5033d3684f9f01cff558c9c7300a1820c
Oeslei Kuhn <oesleik@gmail.com>
2017-06-07 23:38:24 -0300

    Configurações sobre o mapa

--- src/index.js
+++ src/index.js
@@ -10,7 +10,12 @@ function nfov (config) {
     angle: 0,
     angleUnit: nfov.RADIANS,
     orientation: nfov.COUNTERCLOCKWISE,
-    map: null,
+    grid: null,
+    tileSize: {
+      width: 1,
+      height: 1
+    },
+    acceptableTiles: [],
     handler: basicHandler
   }, config)
 
@@ -18,7 +23,9 @@ function nfov (config) {
   this.setAngle(config.angle)
   this.setAngleUnit(config.angleUnit)
   this.setOrientation(config.orientation)
-  this.setMap(config.map)
+  this.setGrid(config.grid)
+  this.setTileSize(config.tileSize)
+  this.setAcceptableTiles(config.acceptableTiles)
   this.setHandler(config.handler)
 }
 
@@ -90,16 +97,50 @@ nfov.prototype.getOrientation = function getOrientation () {
   return this.orientation
 }
 
-nfov.prototype.setMap = function setMap (map) {
-  if (typeof map === 'undefined') {
-    throw new Error('Invalid value passed for `map`')
+nfov.prototype.setGrid = function setGrid (grid) {
+  if (grid == null || Array.isArray(grid)) {
+    this.grid = grid
   } else {
-    this.map = map
+    throw new Error('Invalid value passed for `grid`')
   }
 }
 
-nfov.prototype.getMap = function getMap () {
-  return this.map
+nfov.prototype.getGrid = function getGrid () {
+  return this.grid
+}
+
+nfov.prototype.setTileSize = function setTileSize (widthOrObj, height) {
+  if (widthOrObj > 0) {
+    this.tileSize = {
+      width: widthOrObj,
+      height: height > 0 ? height : widthOrObj
+    }
+  } else if (widthOrObj != null && widthOrObj.width > 0 && widthOrObj.height > 0) {
+    this.tileSize = {
+      width: widthOrObj.width,
+      height: widthOrObj.height
+    }
+  } else {
+    throw new Error('Invalid value passed for `tileSize`')
+  }
+}
+
+nfov.prototype.getTileSize = function getTileSize () {
+  return this.tileSize
+}
+
+nfov.prototype.setAcceptableTiles = function setAcceptableTiles (tiles) {
+  if (Array.isArray(tiles)) {
+    this.acceptableTiles = tiles.slice(0)
+  } else if (tiles != null) {
+    this.acceptableTiles = [tiles]
+  } else {
+    throw new Error('Invalid value passed for `tiles`')
+  }
+}
+
+nfov.prototype.getAcceptableTiles = function getAcceptableTiles () {
+  return this.acceptableTiles
 }
 
 nfov.prototype.setHandler = function setHandler (handler) {
